b6b634d8c488abaf66a0d2699443714453e430dd
Rafael Cortez <rafaelcortezp@gmail.com>
2021-04-18 15:53:31 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-spring-boot-ionic-rafael.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-rafael-cortez.s3-sa-east-1.amazonaws.com"
 }
